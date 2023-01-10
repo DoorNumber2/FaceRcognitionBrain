7669cf0879a23d8aa0572fda0bf1c5c838fbc9d5
guydahang7 <guydahang7@gmail.com>
2023-01-10 16:48:23 +0200

    d

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("https://guarded-reaches-61307.herokuapp.com/imageurl", {
+    fetch("http://localhost:3000/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -76,7 +76,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://guarded-reaches-61307.herokuapp.com/image", {
+          fetch("http://localhost:3000/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
